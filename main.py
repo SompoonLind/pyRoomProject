0e04050b4fc5a8828d3fe2039c447b7c068183eb
Sompoon <alind19@student.aau.dk>
2022-05-04 13:42:39 +0200

    ez clap

--- main.py
+++ main.py
@@ -1,41 +1,31 @@
 import numpy as np
-import pyroomacoustics as pra
-from scipy.io import wavfile
-
-rt60 = 0.38  # Seconds
-
-#       Room Dimensions         #
-roomYogaDim = [6, 5, 3.5]
-roomShopDim = [5.5, 4, 3.5]
-roomReceptionDim = [5.5, 4, 3.5]
-roomSaunaDim = [6, 4, 3.5]
-roomGymDim = [6, 10, 3.5]
-roomSwimmingPoolDim = [10, 8, 5]
-roomJacuzziDim = [6, 8, 3.5]
-roomToiletsDim = [6, 3, 3]
-roomCafeDim = [12, 4, 3.5]
-roomLockerDim = [6, 5, 3]
-roomBarDim = [12, 6, 3.5]
-micLoc = np.c_[
-    [3, 2.5, 3]
-]
-
-audio = wavfile.read('HolySteel.wav')
-
-e_absorption, maxOrder = pra.inverse_sabine(rt60, roomBarDim)
-roomBar = pra.ShoeBox(roomBarDim, fs=16000, materials=pra.Material(e_absorption))
-roomBar.add_source([3, 2.5, 1.8], signal=audio, delay=1.3)
-roomBar.add_microphone_array(micLoc)
-
-roomBar.mic_array.to_wav(
-    f"HolySteel.wav",
-    norm=True,
-    bitdepth=np.int16,
-)
-
-roomBar.compute_rir()
-
+import scipy.signal as ss
+import soundfile as sf
+import rir_generator as rir
 import matplotlib.pyplot as plt
 
-plt.plot(roomBar.rir[1][0])
-plt.show()
+signal, fs = sf.read("HolySteel.wav", always_2d=True)
+
+h = rir.generate(
+    c=340,                  # Sound velocity (m/s)
+    fs=fs,                  # Sample frequency (samples/s)
+    r=[                     # Receiver position(s) [x y z] (m)
+        [0.1, 2.5, 1.8],
+        [0.1, 5, 1.8],
+        [3, 5, 1.8],
+    ],
+    s=[3, 2.5, 1.8],          # Source position [x y z] (m)
+    L=[6, 5, 3.5],            # Room dimensions [x y z] (m)
+    reverberation_time=0.38, # Reverberation time (s)
+    nsample=4096,           # Number of output samples
+)
+
+print(h.shape)              # (4096, 3)
+print(signal.shape)         # (11462, 2)
+
+# Convolve 2-channel signal with 3 impulse responses
+signal = ss.convolve(h[:, None, :], signal[:, :, None])
+
+print(signal.shape)         # (15557, 2, 3)
+
+plt.plot
---
+++ pyRoomMain.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyroomacoustics as pra
+from scipy.io import wavfile
+
+# The desired reverberation time and dimensions of the room
+rt60_tgt = (0.3577777777777778)  # seconds
+room_dim = [6, 3, 4]  # meters
+
+# import a mono wavfile as the source signal
+# the sampling frequency should match that of the room
+fs, audio = wavfile.read("Beep.wav")
+
+# We invert Sabine's formula to obtain the parameters for the ISM simulator
+#maxOrder = pra.inverse_sabine(rt60_tgt, room_dim)
+m = pra.make_materials(
+    ceiling="hard_surface",
+    floor="ceramic_tiles",
+    east="ceramic_tiles",
+    west="ceramic_tiles",
+    north="ceramic_tiles",
+    south="ceramic_tiles",
+)
+# Create the room
+room = pra.ShoeBox(
+    room_dim, fs=fs, materials=m, max_order=17, air_absorption=True, ray_tracing=True
+)
+
+# place the source in the room
+room.add_source([3, 1.5, 1.8], signal=audio, delay=0.5)
+
+# define the locations of the microphones
+mic_locs = np.c_[
+    [3, 3, 1.8],
+    [6, 3, 1.8],
+    [6, 1.5, 1.8],
+]
+
+# finally place the array in the room
+room.add_microphone_array(mic_locs)
+
+# Run the simulation (this will also build the RIR automatically)
+room.simulate()
+
+room.mic_array.to_wav(
+    f"BeepToilet.wav",
+    norm=True,
+    bitdepth=np.int16,
+)
+
+# measure the reverberation time
+rt60 = room.measure_rt60()
+print("The desired RT60 was {}".format(rt60_tgt))
+print("The measured RT60 is {}".format(rt60[1, 0]))
+
+# Create a plot
+plt.figure()
+
+# plot one of the RIR. both can also be plotted using room.plot_rir()
+rir_1_0 = room.rir[1][0]
+plt.subplot(2, 1, 1)
+plt.plot(np.arange(len(rir_1_0)) / room.fs, rir_1_0)
+plt.title("The RIR from source 0 to mic 1")
+plt.xlabel("Time [s]")
+
+# plot signal at microphone 1
+plt.subplot(2, 1, 2)
+plt.plot(room.mic_array.signals[1, :])
+plt.title("Microphone 1 signal")
+plt.xlabel("Time [s]")
+
+plt.tight_layout()
+plt.show()
